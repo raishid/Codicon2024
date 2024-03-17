d6c65a13f57b96619f4eed704c24a6feb1e2e8f3
Oswaldo <oswaldosc1117@gmail.com>
2024-03-17 04:11:04 -0400

    Paquetes, descripcion de los eventos, retoques finales

--- frontend/src/components/Paquetes/Paquetes.tsx
+++ frontend/src/components/Paquetes/Paquetes.tsx
@@ -1,5 +1,50 @@
 export const Paquetes = () => {
-  return (
-    <div>Paquetes</div>
-  )
+    return (
+        <main className="contenedor">
+            <h2 className="heading">Paquetes TitanScript</h2>
+            <p className="descripcion">Compara los paquetes de TitanScript</p>
+
+            <div className="paquetes__grid">
+                <div className="paquete">
+                    <div className="paquete-ordenar">
+                        <h3 className="paquete__nombre">Pase Gratis</h3>
+                        <ul className="paquete__lista">
+                            <li className="paquete__elemento">Acceso Virtual a TitanScript</li>
+                        </ul>
+                    </div>
+                        <p className="paquete__precio">$0</p>
+                </div>
+
+                <div className="paquete">
+                    <div className="paquete-ordenar">
+                        <h3 className="paquete__nombre">Pase Presencial</h3>
+                        <ul className="paquete__lista">
+                            <li className="paquete__elemento">Acceso Presencial a TitanScript</li>
+                            <li className="paquete__elemento">Pase por dos días</li>
+                            <li className="paquete__elemento">Acceso Workshops y Conferencias</li>
+                            <li className="paquete__elemento">Acceso a las grabaciones</li>
+                            <li className="paquete__elemento">Merchandising del Evento</li>
+                            <li className="paquete__elemento">Comida y bebida</li>
+                        </ul>
+                    </div>
+
+                    <p className="paquete__precio">$199</p>
+                </div>
+
+                <div className="paquete">
+                    <div className="paquete-ordenar">
+                        <h3 className="paquete__nombre">Pase Virtual</h3>
+                        <ul className="paquete__lista">
+                            <li className="paquete__elemento">Acceso Virtual a TitanScript</li>
+                            <li className="paquete__elemento">Pase por dos días</li>
+                            <li className="paquete__elemento">Enlaces a Workshops y Conferencias</li>
+                            <li className="paquete__elemento">Acceso a las grabaciones</li>
+                        </ul>
+                    </div>
+
+                    <p className="paquete__precio">$49</p>
+                </div>
+            </div>
+        </main>
+    )
 }
--- frontend/src/components/conferencias/Conferencias.constans.ts
+++ frontend/src/components/conferencias/Conferencias.constans.ts
@@ -1,79 +1,79 @@
 export const conferencias = [
     {
-        date: "Viernes 22 de Marzo",
+        date: "Viernes 22 de Marzo - Frontend",
         ponentes:[
             {
                 hora: "10:00am - 10:55am",
-                title: "Arquitecturas de Microservicios: Ventajas y Desafíos",
-                description: 'Descripcion del ponente',
-                name: 'Giusseppe Perretti',
+                title: "Optimización de Rendimiento en Aplicaciones Web",
+                description: 'Carga rápida de páginas, reducción del tiempo de respuesta del servidor, optimización de recursos (como imágenes y scripts), caché, compresión y más.',
+                name: 'Alba Castro',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
         
             {
                 hora: "11:00am - 11:55am",
-                title: "Seguridad en APIs RESTful: Autenticación y Autorización",
-                description: 'Descripcion del ponente',
-                name: 'Hector Gallardo',
+                title: "Diseño Responsivo: Más Allá de los Media Queries",
+                description: 'Más allá de simplemente adaptar el diseño a diferentes tamaños de pantalla, profundizaremos en estrategias avanzadas para crear experiencias fluidas y consistentes en dispositivos móviles, tabletas y computadoras de escritorio',
+                name: 'Orlando Sánchez',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
         
             {
                 hora: "1:00pm - 1:55pm",
-                title: "Bases de Datos NoSQL vs. SQL: ¿Cuándo Usar Cada Una?",
-                description: 'Descripcion del ponente',
-                name: 'Haby Rivas',
+                title: "Explorando las Nuevas Características de CSS Grid",
+                description: 'Nos sumergiremos en las características avanzadas de CSS Grid: Grid Layout Fundamentals, nuevas características, optimización y buenas prácticas',
+                name: 'Stephanie Molina',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
         
             {
                 hora: "2:00pm - 2:55pm",
-                title: "Escalabilidad Horizontal vs. Vertical en Servidores",
-                description: 'Descripcion del ponente',
-                name: 'Daniel Gonzalez',
+                title: "Mejores Prácticas en Desarrollo de Interfaces de Usuario",
+                description: 'Estrategias y técnicas fundamentales para crear interfaces de usuario efectivas y atractivas, diseño centrado en el usuario, accesibilidad, diseño inclusivo, consistencia visual y patrones de diseño',
+                name: 'Rosana Mariño',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
         
             {
                 hora: "3:00pm - 3:55pm",
-                title: "Introducción a GraphQL y su Implementación en el Backend",
-                description: 'Descripcion del ponente',
-                name: 'Jesus Falcon',
+                title: "Introducción a Progressive Web Apps (PWAs)",
+                description: 'Características y ventajas de las Progressive Web Apps (PWAs). Combinando lo mejor de las aplicaciones web y las aplicaciones nativas.',
+                name: 'Federico David',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
         
             {
                 hora: "4:00pm - 4:55pm",
-                title: "Automatización de Tareas con Scripts en el Backend",
-                description: 'Descripcion del ponente',
-                name: 'Dennys Avila',
+                title: "Accesibilidad Web: Diseñando para Todos",
+                description: 'La importancia de diseñar interfaces web accesibles para todas las personas, incluyendo aquellas con discapacidades.',
+                name: 'Wilfredo Bracho',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
         
             {
                 hora: "5:00pm - 5:55pm",
-                title: "Optimización de Consultas SQL: Estrategias Avanzadas",
-                description: 'Descripcion del ponente',
-                name: 'Froilan Estaba',
+                title: "Animaciones y Transiciones CSS: Crea Experiencias Visuales Impactantes",
+                description: 'Exploraremos cómo las animaciones y transiciones CSS pueden transformar la apariencia y la interacción en sitios web y aplicaciones.',
+                name: 'Sebastián Sanabria',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
         
             {
                 hora: "6:00pm - 6:55pm",
-                title: "Machine Learning con Python: Conceptos Básicos y Ejemplos Prácticos",
-                description: 'Descripcion del ponente',
-                name: 'Dilia Roa',
+                title: "TypeScript: Más Allá de la Sintaxis",
+                description: 'Adéntrate en las características avanzadas de TypeScript, como tipos condicionales, mapped types y type guards. Destaca cómo estas funcionalidades mejoran la calidad del código.',
+                name: 'Oswaldo Sánchez',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             }
         ]
     },
     {
-        date: "Sábado 23 de Marzo",
+        date: "Sábado 23 de Marzo - Backend",
         ponentes: [
             {
                 hora: "10:00am - 10:55am",
                 title: "Arquitecturas de Microservicios: Ventajas y Desafíos",
-                description: 'Descripcion del ponente',
+                description: 'Las arquitecturas de microservicios ofrecen flexibilidad y escalabilidad. Pero, ¿Cuáles son los desafíos a considerar?',
                 name: 'Giusseppe Perretti',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
@@ -81,7 +81,7 @@ export const conferencias = [
             {
                 hora: "11:00am - 11:55am",
                 title: "Seguridad en APIs RESTful: Autenticación y Autorización",
-                description: 'Descripcion del ponente',
+                description: 'Conocimientos esenciales para garantizar la seguridad en las APIs RESTful y cómo proteger tus aplicaciones contra amenazas.',
                 name: 'Hector Gallardo',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
@@ -89,7 +89,7 @@ export const conferencias = [
             {
                 hora: "1:00pm - 1:55pm",
                 title: "Bases de Datos NoSQL vs. SQL: ¿Cuándo Usar Cada Una?",
-                description: 'Descripcion del ponente',
+                description: 'Compararemos las bases de datos NoSQL y SQL. Discutiremos sus diferencias, ventajas y casos de uso apropiados para cada una.',
                 name: 'Haby Rivas',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
@@ -97,7 +97,7 @@ export const conferencias = [
             {
                 hora: "2:00pm - 2:55pm",
                 title: "Escalabilidad Horizontal vs. Vertical en Servidores",
-                description: 'Descripcion del ponente',
+                description: 'Diferencias entre escalabilidad horizontal y vertical en el contexto de servidores. Discutiremos cuándo es apropiado aplicar cada enfoque y cómo afectan el rendimiento, la disponibilidad y los costos.',
                 name: 'Daniel Gonzalez',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
@@ -105,7 +105,7 @@ export const conferencias = [
             {
                 hora: "3:00pm - 3:55pm",
                 title: "Introducción a GraphQL y su Implementación en el Backend",
-                description: 'Descripcion del ponente',
+                description: 'Cómo funciona, sus ventajas sobre REST, y cómo implementarlo para crear APIs flexibles y eficientes.',
                 name: 'Jesus Falcon',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
@@ -113,7 +113,7 @@ export const conferencias = [
             {
                 hora: "4:00pm - 4:55pm",
                 title: "Automatización de Tareas con Scripts en el Backend",
-                description: 'Descripcion del ponente',
+                description: 'Cómo crear scripts eficientes para procesos como migraciones de bases de datos, programar de tareas recurrentes y manejar de flujos de trabajo.',
                 name: 'Dennys Avila',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
@@ -121,7 +121,7 @@ export const conferencias = [
             {
                 hora: "5:00pm - 5:55pm",
                 title: "Optimización de Consultas SQL: Estrategias Avanzadas",
-                description: 'Descripcion del ponente',
+                description: 'Índices, subconsultas, uso eficiente de JOINs y técnicas para reducir la carga en servidores.',
                 name: 'Froilan Estaba',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             },
@@ -129,7 +129,7 @@ export const conferencias = [
             {
                 hora: "6:00pm - 6:55pm",
                 title: "Machine Learning con Python: Conceptos Básicos y Ejemplos Prácticos",
-                description: 'Descripcion del ponente',
+                description: 'Algoritmos de clasificación, regresión y agrupamiento. Además, presentaremos ejemplos prácticos de aplicaciones de Machine Learning en la vida real.',
                 name: 'Dilia Roa',
                 imagen: 'https://c0.wallpaperflare.com/preview/115/959/619/italy-genova-sestri-ponente-tramonto.jpg',
             }
